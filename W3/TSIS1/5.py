fac6ab36cb7d6ff03ea5a249de49d229dff6422f
Amtilan <153347359+dolphilina@users.noreply.github.com>
2024-02-09 23:55:57 +0600

    TSIS1

--- W3/TSIS1/5.py
+++ W3/TSIS1/5.py
@@ -2,25 +2,38 @@ class Account():
     def __init__(self, owner):
         self.owner = owner
         self.balance = 0
-
+    """
+    __init__: Метод-конструктор класса, 
+    который инициализирует атрибуты счета при его создании: 
+    owner (владелец счета) и balance (баланс счета, изначально 0).
+    """
     def checkBal(self):
         print(f"Balance is {self.balance}")
-
+    #checkBal: Метод, который выводит текущий баланс счета.
     def deposit(self, amount):
         self.balance += amount
         print(f"{amount} has been deposited")
-
+    """
+    deposit: Метод, который позволяет владельцу счета 
+    внести деньги на счет (увеличивает баланс на указанную сумму).
+    """
     def withdraw(self, amount):
         if amount > self.balance:
             print("Not enough money on balance")
         else:
             self.balance -= amount
             print(f"{amount} has been withdrawn from deposit")
+    """
+    withdraw: Метод, который позволяет владельцу счета снять деньги 
+    cо счета (уменьшает баланс на указанную сумму), 
+    предварительно проверяя, 
+    достаточно ли денег на счете для снятия заданной суммы.
+    """
 
-
-own1 = Account("Kama^^")
-own1.checkBal()
-own1.deposit(5000)
-own1.checkBal()
-own1.withdraw(2000)
-own1.checkBal()
+# Далее, создается объект own1 класса Account с владельцем "Yipppeeee". Затем вызываются методы этого объекта для демонстрации его функциональности:
+own1 = Account("Yipppeeee")
+own1.checkBal() #checkBal(): Печатает начальный баланс счета (0).
+own1.deposit(5000) #deposit(5000): Вносит на счет 5000 денег.
+own1.checkBal() #checkBal(): Печатает текущий баланс счета (5000).
+own1.withdraw(2000) #withdraw(2000): Снимает со счета 2000 денег.
+own1.checkBal() #checkBal(): Печатает текущий баланс счета после снятия (3000).
--- W3/TSIS1/6.py
+++ W3/TSIS1/6.py
@@ -1,4 +1,5 @@
 class Filter_prime():
+    
     def isPrime(self, num):
         if (num < 2):
             return False
@@ -7,11 +8,29 @@ class Filter_prime():
                 if(num % i == 0):
                     return False
         return True   
-
+    """
+    isPrime(self, num): Метод, который проверяет, 
+    является ли число простым. Он возвращает True, 
+    если число простое, и False в противном случае. 
+    Для проверки простоты числа используется 
+    метод перебора делителей.
+    """
     def filter_primes(self, listofnums):
         return filter(lambda x : self.isPrime(x), listofnums)
-
-prime_filter = Filter_prime()
+    """
+    filter_primes(self, listofnums): Метод, который фильтрует 
+    список чисел, оставляя только простые числа. 
+    Он использует функцию filter(), 
+    передавая в неё лямбда-функцию, 
+    которая вызывает isPrime() для каждого элемента списка.
+    """
+prime_filter = Filter_prime() #Создаётся объект prime_filter типа Filter_prime.
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 prime_numbers = list(prime_filter.filter_primes(nums))
-print(prime_numbers)
+"""
+Применяется метод filter_primes() объекта prime_filter 
+к списку nums. Он фильтрует список, 
+оставляя только простые числа, 
+и результат сохраняется в переменной prime_numbers.
+"""
+print(prime_numbers) #[2, 3, 5, 7]
